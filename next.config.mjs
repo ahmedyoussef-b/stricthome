4c9102e8fc926dc96d7b1d65e369415d52947365
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-02 16:26:22 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1759130826485.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev"],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
