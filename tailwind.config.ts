7741f56b75693c20b2a6ce86eb3c6e04e146955b
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-04 08:59:15 +0000

    diminue la frequance des clignotement

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,7 +96,7 @@ module.exports = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'background-shine': 'background-shine 2s linear infinite',
+        'background-shine': 'background-shine 4s linear infinite',
       },
     },
   },
