e8f3e51f86f3f64181f7e0c1561bb848c14f4f61
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-05 23:13:31 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={inter.className}
-      >
+      <body className={inter.className}>
         <AuthProvider>
           {children}
           {modal}
