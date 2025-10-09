6a14e824eaf7d87c303908bcc241692bad034372
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-09 20:50:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: '/_next/static/favicon.ico',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
