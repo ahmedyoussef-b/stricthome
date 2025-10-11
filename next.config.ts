4b71fafc0aa031393d898428a6ce5fe56b8140bd
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-11 00:07:44 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -25,12 +25,6 @@ const nextConfig = {
   allowedDevOrigins: [
     "https://6000-firebase-studio-1759130826485.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev"
   ],
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.cache = false;
-    }
-    return config;
-  },
   experimental: {
   }
 }
