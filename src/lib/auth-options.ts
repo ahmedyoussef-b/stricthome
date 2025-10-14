8c8eecd95614d7834eb0829cccc932089377fba1
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-14 14:55:41 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -1,6 +1,6 @@
 // src/lib/auth-options.ts
 import { NextAuthOptions } from 'next-auth';
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
 import prisma from '@/lib/prisma';
