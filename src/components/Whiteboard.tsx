78e730044dd925ff1a5cd31f2aa8062bcd7179a4
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-16 11:09:28 +0000

    [{ 	"resource": "/home/user/studio/src/components/Whiteboard.tsx", 	"own

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -1,30 +1,16 @@
 // src/components/Whiteboard.tsx
 'use client';
 
-import { Tldraw, useEditor } from '@tldraw/tldraw';
+import { Tldraw } from '@tldraw/tldraw';
 import '@tldraw/tldraw/tldraw.css';
 import { Card } from './ui/card';
-import { useEffect } from 'react';
 import { FrenchTranslation } from '@/lib/tldraw-fr';
-
-
-function EditorWrapper() {
-    const editor = useEditor();
-    useEffect(() => {
-        editor.updateInstanceState({
-            locale: 'fr',
-        });
-    }, [editor]);
-    return null;
-}
-
 
 export function Whiteboard() {
   return (
     <Card className="w-full h-full flex-1 relative bg-background/80">
-      <Tldraw persistenceKey="classroom-whiteboard" assetUrls={{}} >
-         <EditorWrapper />
-         <FrenchTranslation />
+      <Tldraw persistenceKey="classroom-whiteboard" assetUrls={{}}>
+        <FrenchTranslation />
       </Tldraw>
     </Card>
   );
