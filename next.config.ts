36d5d64c7eb9ceee009fbddbca68c258f7c549b7
bee bees <ahmedyoussefabbes@gmail.com>
2025-10-18 12:36:52 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['embla-carousel-react', '@next-auth/prisma-adapter', 'next-auth'],
+  compress: false,
   images: {
     remotePatterns: [
       {
